7ecc553abd845ba593c5437848ddc8c2e657c7f8
HR <hunglq@rikkeisoft.com>
2017-03-13 08:47:06 +0700

    add curl

---
+++ provision/scripts/install_curl-php5.6.rb
@@ -0,0 +1,7 @@
+#======================================
+# Lamp-Vagrant
+# @copyright : Dumday (c) 2017
+#======================================
+command = LampVagrant.command
+command.push_install_message(["CURL for PHP 5.6"])
+command.push_install(["php5.6-curl"])
